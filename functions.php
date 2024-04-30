fdfd3731c8b6bd485b8b56a9de41fcf63f2a7909
Merrrrrrry <grs.merry@gmail.com>
2024-04-30 14:35:14 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+function clobotics_register_stylesheet() {
+    //wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css");
+    
+    wp_enqueue_style("sitesecurity-style", get_stylesheet_directory_uri() . "/style.css");
+    wp_enqueue_style("materialize-icons", "https://fonts.googleapis.com/icon?family=Material+Icons");
+    wp_enqueue_style("din_condensed_font", "https://use.typekit.net/piy2rgn.css");
+}
+add_action("wp_enqueue_scripts", "clobotics_register_stylesheet");
+
+
+// Disable Gutenberg
+function disable_gutenberg() {
+    remove_post_type_support("page", "editor");
+    remove_post_type_support("post", "editor");
+}
+add_action("init", "disable_gutenberg");
+
+
+//echo '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header() ?>
+
+
+    <?php while(have_posts()): the_post() ?>
+        <?php the_content() ?>
+    <?php endwhile ?>
+
+<?php get_footer() ?>
+
+
+<body>
+
+
+
+</body>
