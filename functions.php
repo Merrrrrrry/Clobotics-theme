e2e90ba46f82464acc47d7b1e248f57c97a3a82f
GabrielaLukacova <gabluk01@easv365.dk>
2024-05-02 12:52:22 +0200

    menu
    menu

--- functions.php
+++ functions.php
@@ -2,10 +2,9 @@
 function clobotics_register_stylesheet() {
     //wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css");
     
-    wp_enqueue_style("sitesecurity-style", get_stylesheet_directory_uri() . "/style.css");
+    wp_enqueue_style("clobotics-style", get_stylesheet_directory_uri() . "/style.css");
     wp_enqueue_style("materialize-icons", "https://fonts.googleapis.com/icon?family=Material+Icons");
-    wp_enqueue_style("din_condensed_font", "https://use.typekit.net/piy2rgn.css");
-    wp_enqueue_script("clobotics", get_stylesheet_directory_uri() . "/javaskript.js");
+    wp_enqueue_script("clobotics", get_stylesheet_directory_uri() . "/javascript.js");
 }
 add_action("wp_enqueue_scripts", "clobotics_register_stylesheet");
 
@@ -16,6 +15,17 @@ function disable_gutenberg() {
     remove_post_type_support("post", "editor");
 }
 add_action("init", "disable_gutenberg");
+
+
+
+// Displaying menu under Appearance in WP backhand 
+<?php
+function demo_register_menus() {
+    register_nav_menus(array(
+        "main-menus" => "Main Menu Location"
+    ));
+}
+add_action("init", "demo_register_menus");
 
 
 //echo '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
--- header.php
+++ header.php
@@ -7,3 +7,5 @@
     <?php wp_head() ?>
 </head>
 <body>
+
+<?php wp_nav_menu(array("theme_location"=> "main-menu")) ?>
