6776aa15d2907b4b404cbcfcb1d25d48a134f66d
GabrielaLukacova <gabluk01@easv365.dk>
2024-05-02 14:20:10 +0200

    js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
-
-
-    function showInfo() {
-        var infoText = document.getElementById("info-text");
+function showInfo() {
+    var infoText = document.getElementById("info-text");
+    if (infoText.style.display === "none") {
         infoText.style.display = "block";
+    } else {
+        infoText.style.display = "none";
     }
+}
