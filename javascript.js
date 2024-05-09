9f4f1fd6dcb91cd5cb2a7603e68772ca98cd40c6
GabrielaLukacova <gabluk01@easv365.dk>
2024-05-09 14:26:41 +0200

    font

--- javascript.js
+++ javascript.js
@@ -8,9 +8,3 @@ function showInfo() {
 }
 
 
-//FONT
-
-function enqueue_google_fonts() {
-    wp_enqueue_style( 'open-sans', 'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap' );
-}
-add_action( 'wp_enqueue_scripts', 'enqueue_google_fonts' );
