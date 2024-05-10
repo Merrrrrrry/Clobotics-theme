01c355797b6de673d59ef883d4c77d761d09e8dd
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-10 17:54:07 +0200

    br

--- page-contact.php
+++ page-contact.php
@@ -17,7 +17,7 @@ Template Name: Contact
                     <img class="image-hero" src="<?php echo get_template_directory_uri(); ?>/media/11.png" alt="Your Image Alt Text">
              </div>
                     <div class="hero-section-content">
-                        <h1 class="hero-title">Meet Clobotics - a vision technology company with offices all around the world</h1>
+                        <h1 class="hero-title">Meet Clobotics - a vision technology company <br> with offices all around the world</h1>
                 </div>
          </div>
 
