9bfc83f718ce4e073738d2abd3feb24bf081e808
Merrrrrrry <grs.merry@gmail.com>
2024-05-10 11:27:46 +0200

    title class

--- page-about_us.php
+++ page-about_us.php
@@ -33,7 +33,7 @@ Template Name: About us
 </div>
 
     <div class="hero-section-content">
-        <h1 class="hero page-title"> Meet Clobotics - vision technology company with offices all around the world </h1>
+        <h1 class="hero title"> Meet Clobotics - vision technology company with offices all around the world </h1>
         <p class="hero page-slogan"> </p>
     </div>
 </div>
