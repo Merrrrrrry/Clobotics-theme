b4cceead4dccf235685e1b0a0c2f647946206dfd
Merrrrrrry <grs.merry@gmail.com>
2024-05-12 22:11:39 +0200

    team loop

--- page-about_us.php
+++ page-about_us.php
@@ -85,11 +85,10 @@ Template Name: About us
 
             <div class="meet-our-team_main_content">
                 <div class="meet-our-team-box-content">
-                    <div style="background-image: <?php $image = get_field('portrait_image'); ?> url('<?php echo esc_url($image["url"]); ?>'); background-position: auto; background-repeat: no-repeat; width: 80px; height: 80px;" ></div>
-                        <p class="title thin" style="margin-right: 30px;" ><?php echo get_field("name_and_surname"); ?></p>
-                        <p class="subtitle" style="fond-size: 24px; margin-right: 30px;" ><?php echo get_field("job_position"); ?></p>
-                        <p style="margin-right: 30px;" ><?php echo get_field("name_and_surname"); ?></p>
-                    </div>
+                    <img src="'<?php echo esc_url($image["url"]); ?>" alt="Portrait of <?php echo get_field("name_and_surname"); ?>">
+                    <p class="title thin" style="margin-right: 30px;" ><?php echo get_field("name_and_surname"); ?></p>
+                    <p class="subtitle" style="fond-size: 24px; margin-right: 30px;" ><?php echo get_field("job_position"); ?></p>
+                    <p style="margin-right: 30px;" ><?php echo get_field("name_and_surname"); ?></p>
                 </div>
             </div>
         <?php endwhile; wp_reset_query(); ?>
