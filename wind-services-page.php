10010cfca418881dd4199e6a0a6683144101865a
Merrrrrrry <grs.merry@gmail.com>
2024-05-12 23:43:55 +0200

    dbt

--- wind-services-page.php
+++ wind-services-page.php
@@ -24,13 +24,13 @@ Template Name: Wind services
         </div> -->
 
 
-<!-- Hero Video (referenced from busters world) -->
-<div class="hero-section-background">
-    <video autoplay muted loop class="video-background"> 
-        <source src="<?php echo get_template_directory_uri(); ?> /....../..... .mp4" type="video/mp4">
-    </video>
+    <!-- Hero Video (referenced from busters world) -->
+    <div class="hero-section-background">
+        <video autoplay muted loop class="video-background"> 
+            <source src="<?php echo get_template_directory_uri(); ?> /....../..... .mp4" type="video/mp4">
+        </video>
 
-</div>
+    </div>
 
     <div class="hero-section-content">
         <h1 class="hero page-title"> Wind services </h1>
@@ -53,9 +53,9 @@ Template Name: Wind services
     <h2 class="article_title"><?php the_field('article_title_we_operate_globally'); ?></h2>
     <p class="subtitle"><?php the_field('article_text_we_operate_globally'); ?></p>
 
-        <img class="we_operate_globally-img" id="image_of_our_offices" src="<?php $image = the_field('image_of_our_offices_we_operate_globally_section');  echo esc_url($image["url"]); ?>" alt="Image of our offices (We operate globally section)"> 
-        <img class="we_operate_globally-img" id="image_of_our_partners" src="<?php $image = the_field('image_of_our_partners_we_operate_globally_section');  echo esc_url($image["url"]); ?>" alt="Image of our partners (We operate globally section)"> 
-        <img class="we_operate_globally-img" id="image_of_our_work" src="<?php $image = the_field('image_of_our_work_we_operate_globally_section');  echo esc_url($image["url"]); ?>" alt="Image of our work (We operate globally section)"> 
+        <img class="we_operate_globally-img" id="image_of_our_offices" src="<?php $image = the_field('image_of_our_offices_we_operate_globally_section');  echo esc_url($image["url"]); ?>" alt="Image of our offices (We operate globally section)" /> 
+        <img class="we_operate_globally-img" id="image_of_our_partners" src="<?php $image = the_field('image_of_our_partners_we_operate_globally_section');  echo esc_url($image["url"]); ?>" alt="Image of our partners (We operate globally section)" /> 
+        <img class="we_operate_globally-img" id="image_of_our_work" src="<?php $image = the_field('image_of_our_work_we_operate_globally_section');  echo esc_url($image["url"]); ?>" alt="Image of our work (We operate globally section)" /> 
 
 </div>
 
@@ -65,5 +65,5 @@ Template Name: Wind services
     <video autoplay class="video-background"> 
         <source src="<?php echo get_template_directory_uri(); ?> /....../..... .mp4" type="video/mp4">
     </video>
-
 </div>
+<?php get_footer(); ?>
