2078864e37bb690a3bd2ad916c1194e72188c100
Merrrrrrry <grs.merry@gmail.com>
2024-05-13 23:17:12 +0200

    anti cache

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 function clobotics_register_stylesheet() {
     //wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css");
     
-    wp_enqueue_style("clobotics-style", get_stylesheet_directory_uri() . "/style.css" . "?v".mktime());
+    wp_enqueue_style("clobotics-style", get_stylesheet_directory_uri() . "/style.css"."?v".mktime());
+    wp_enqueue_script("clobotics", get_stylesheet_directory_uri() . "/javascript.js"."?v".mktime());
     wp_enqueue_style("materialize-icons", "https://fonts.googleapis.com/icon?family=Material+Icons");
-    wp_enqueue_script("clobotics", get_stylesheet_directory_uri() . "/javascript.js");
 }
 add_action("wp_enqueue_scripts", "clobotics_register_stylesheet");
 
