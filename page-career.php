3da4592dcee134770f1f77110b78919a59d49cc0
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-13 19:17:04 +0200

    career

--- page-career.php
+++ page-career.php
@@ -28,43 +28,44 @@ Template Name: Career
         </div>
 
         <!-- Open Positions section/Job list -->
-        <div class="open-positions">
-            <h2 class="title">Open Positions</h2>
+<div class="open-positions">
+    <h2 class="title">Open Positions</h2>
 
-              <!-- Gap -->
-        <div style="height: 10px;"></div>
+    <!-- Gap -->
+    <div style="height: 10px;"></div>
 
-            <?php
-            $search_query = isset($_GET['search_query']) ? sanitize_text_field($_GET['search_query']) : '';
-            $args = array(
-                'post_type' => 'open-position',
-                'posts_per_page' => -1,
-                's' => $search_query,
-            );
-            $related_positions = new WP_Query($args);
+    <?php
+    $search_query = isset($_GET['search_query']) ? sanitize_text_field($_GET['search_query']) : '';
+    $args = array(
+        'post_type' => 'open-position',
+        'posts_per_page' => -1,
+        's' => $search_query,
+    );
+    $related_positions = new WP_Query($args);
 
-            if ($related_positions->have_posts()) : ?>
-                <ul class="position-list">
-                    <?php while ($related_positions->have_posts()) : $related_positions->the_post(); ?>
-                        <li class="position-item">
-                            <h3 class="job-title"><?php the_field('job_title'); ?></h3>
-                            <p class="job-subtitle">
-                                <?php
-                                $job_location = get_field('job_location');
-                                $job_type = get_field('job_type');
-                                echo $job_location . ' | ' . $job_type;
-                                ?>
-                                <a href="<?php the_permalink(); ?>" class="btn">Learn more</a>
-                            </p>
-                        </li>
-                    <?php endwhile; ?>
-                </ul>
-                <?php
-                wp_reset_postdata();
-            else : ?>
-                <p class="job-subtitle">No open positions found for "<?php echo $search_query; ?>"</p>
-            <?php endif; ?>
-        </div>
+    if ($related_positions->have_posts()) : ?>
+        <ul class="position-list">
+            <?php while ($related_positions->have_posts()) : $related_positions->the_post(); ?>
+                <li class="position-item">
+                    <h3 class="job-title"><?php the_field('job_title'); ?></h3>
+                    <p class="job-subtitle">
+                        <?php
+                        $job_location = get_field('job_location');
+                        $job_type = get_field('job_type');
+                        echo $job_location . ' <span style="margin-right: 50px;"></span>' . $job_type;
+                        ?>
+                        <a href="<?php the_permalink(); ?>" class="btn" style="float: right;">Learn more</a>
+                    </p>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+        <?php
+        wp_reset_postdata();
+    else : ?>
+        <p class="job-subtitle">No open positions found for "<?php echo $search_query; ?>"</p>
+    <?php endif; ?>
+</div>
+
 
         <!-- Gap between sections -->
         <div style="height: 100px;"></div>
