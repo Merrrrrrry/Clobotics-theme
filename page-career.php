864fccb06cc70b1983abc8ff4a4d0cebc7a067fb
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-14 00:42:59 +0200

    t

--- page-career.php
+++ page-career.php
@@ -83,7 +83,14 @@ Template Name: Career
 
 
 
-            <!-- United Numbers Career section -->
+             <?php
+                $team = get_field('team');
+                $team_members = get_field('team_members');
+                $countries = get_field('countries');
+                $cities = get_field('cities');
+            ?>
+
+                    <!-- United Numbers Career section -->
             <div class="numbers-row">
                 <div class="number-container">
                     <h4 class="number"><?php echo $team; ?></h4>
@@ -92,8 +99,8 @@ Template Name: Career
             <div class="number-container">
                     <h4 class="number"><?php echo $team_members; ?></h4>
                     <h4 class="text">Team Members</h4>
-            </div>
-                 <div class="number-container">
+                </div>
+            <div class="number-container">
                     <h4 class="number"><?php echo $countries; ?></h4>
                     <h4 class="text">Countries</h4>
             </div>
@@ -101,7 +108,7 @@ Template Name: Career
                     <h4 class="number"><?php echo $cities; ?></h4>
                     <h4 class="text">Cities</h4>
             </div>
-            </div>
+        </div>
 
 
 
