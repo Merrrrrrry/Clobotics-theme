d87bc96302c51edf17507425f87c44531f0f7abe
GabrielaLukacova <gabluk01@easv365.dk>
2024-05-14 16:08:51 +0200

    icon size

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@
     
     <div class="social-icons">
         <a href="https://www.linkedin.com/company/cloboticswindservices/">
-            <img src="<?php echo get_template_directory_uri(); ?>/media/linkedin-logo.png" width="45px" height="45px" alt="LinkedIn"> <br>
+            <img src="<?php echo get_template_directory_uri(); ?>/media/linkedin-logo.png"  alt="LinkedIn"> <br>
             <span>Wind</span>
         </a>
         <a href="https://www.linkedin.com/company/clobotics/">
