a6aaf47924d3c59569d1d3a50552a5444053d41f
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-14 11:44:53 +0200

    career

--- single-open-position.php
+++ single-open-position.php
@@ -7,48 +7,45 @@ Template Name: single-open-position
 <?php get_header(); ?>
 
 <main>
-
-   <!-- Hero section -->
-<div class="hero-section">
-    <div class="hero-section-background">
-        <img class="image-hero" src="<?php echo get_template_directory_uri(); ?>/media/11.png" alt="Your Image Alt Text">
+    <!-- Hero section -->
+    <div class="hero-section">
+        <div class="hero-section-background">
+            <img class="image-hero" src="<?php echo get_template_directory_uri(); ?>/media/11.png" alt="Your Image Alt Text">
+        </div>
+        <div class="hero-section-content">
+            <h1 class="hero-title"><?php the_field('job_title'); ?></h1>
+        </div>
     </div>
-    <div class="hero-section-content">
-        <h1 class="hero-title"><?php the_field('job_title'); ?></h1>
-    </div>
-</div>
 
-<a id="apply-button" href="#" class="btn">Apply</a>
-
+    <a id="apply-button" href="#" class="btn">Apply</a>
 
     <!-- Information about the job position -->
-                    <?php while (have_posts()) : the_post(); ?>
-                        <article <?php post_class(); ?>>
-                        <div class="job-info">
-                            <div class="info-item">
-                            <div class="info-label">
-                                 <i class="material-icons">calendar_month</i>
-                                 <span>Deadline</span>
-                            </div>
-                                     <div class="info-value"><?php the_field('deadline'); ?></div>
-                                </div>
-                                    <div class="info-item">
-                                      <div class="info-label">
-                                          <i class="material-icons">work</i>
-                                             <span>Job Type</span>
-                                     </div>
-                             <div class="info-value"><?php the_field('job_type'); ?></div>
-                         </div>
-                            <div class="info-item">
-                            <div class="info-label">
-                                 <i class="material-icons">location_on</i>
-                                <span>Location</span>
-                        </div>
-                            <div class="info-value"><?php the_field('job_location'); ?></div>
-                         </div>
+    <?php while (have_posts()) : the_post(); ?>
+        <article <?php post_class(); ?>>
+            <div class="job-info">
+                <div class="info-item">
+                    <div class="info-label">
+                        <i class="material-icons">calendar_month</i>
+                        <span>Deadline</span>
                     </div>
+                    <div class="info-value"><?php the_field('deadline'); ?></div>
+                </div>
+                <div class="info-item">
+                    <div class="info-label">
+                        <i class="material-icons">work</i>
+                        <span>Job Type</span>
+                    </div>
+                    <div class="info-value"><?php the_field('job_type'); ?></div>
+                </div>
+                <div class="info-item">
+                    <div class="info-label">
+                        <i class="material-icons">location_on</i>
+                        <span>Location</span>
+                    </div>
+                    <div class="info-value"><?php the_field('job_location'); ?></div>
+                </div>
+            </div>
 
-           
             <p><strong><?php the_field('headline-1'); ?></strong></p>
             <p><?php the_field('paragraph-1'); ?></p>
             <p><strong><?php the_field('headline-2'); ?></strong></p>
@@ -62,15 +59,12 @@ Template Name: single-open-position
         <!-- Find more about Clobotics button -->
         <a href="page-about_us.php" class="btn dark">Read more about Clobotics</a>
 
-        <!-- Contact Person -->
-
         <!-- Apply for job contact form -->
         <section id="contact-form-section">
             <div class="contact-form-career">
-                <?php echo do_shortcode( '[contact-form-7 id="c48fb62" title="Apply for the position"]' ); ?>
+                <?php echo do_shortcode('[contact-form-7 id="c48fb62" title="Apply for the position"]'); ?>
             </div>
         </section>
-
     <?php endwhile; ?>
 </main>
 
