b5209348feeb07437b5acaec1198e7b80e474633
GabrielaLukacova <gabluk01@easv365.dk>
2024-05-14 11:52:49 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,42 @@
-<?php wp_footer() ?>
+<footer class="site-footer">
+    <div class="footer-column">
+        <img class="footer-logo" src="/media/11.png" alt="Logo">
+    </div>
+    <div class="footer-column">
+        <h2 class="footer-title">Company</h2>
+        <ul class="footer-menu">
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Media</a></li>
+        </ul>
+    </div>
+    <div class="footer-column">
+        <h2 class="footer-title">Services</h2>
+        <ul class="footer-menu">
+            <li><a href="wind-services-page">Wind</a></li>
+            <li><a href="retail-services-page">Retail</a></li>
+        </ul>
+    </div>
+    <div class="footer-column">
+        <h2 class="footer-title">Get in touch</h2>
+        <a href="contact-us" class="footer-contact-link">Contact us <i class="arrow_right_alt"></i></a>
+    </div>
+    
+    <div class="copyright">
+        © 2024 CLOBOTICS ALL RIGHTS RESERVED - <a href="privacy-cookie-policy">PRIVACY & COOKIE POLICY</a>
+    </div>
+    
+    <div class="social-icons">
+        <img src=".png" alt="LinkedIn">
+        <span>Wind</span>
+        <img src=".png" alt="LinkedIn">
+        <span>Retail</span>
+        <img src="n.png" alt="Vimeo">
+        <span>Vimeo</span>
+    </div>
+</footer>
 
+<?php wp_footer(); ?>
 </body>
 </html>
