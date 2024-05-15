3a17f149fcbbc8e47d4ad57f8e28479699f3cead
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-15 13:49:01 +0200

    privacy n cookie

---
+++ page_privacy-cookie_policy.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Privacy & cookie policy
+*/
+?>
+
+<?php get_header(); ?>
+
+<body>
+    <main>
+        <!-- Hero section -->
+        <div class="hero-section">
+            <div class="hero-section-background">
+                <img class="image-hero" src="<?php echo get_template_directory_uri(); ?>/media/11.png" alt="Your Image Alt Text">
+            </div>
+            <div class="hero-section-content">
+                <h1 class="hero-title">Privacy & Cookie Policy</h1>
+            </div>
+        </div>
+
+        </main>
+</body>
+
+<?php get_footer(); ?>
+
