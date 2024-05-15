af524fcba54c4a1e3be16788c4e0db145d16a8e9
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-15 14:20:11 +0200

    policy n cooki

--- page_privacy-cookie_policy.php
+++ page_privacy-cookie_policy.php
@@ -18,7 +18,22 @@ Template Name: Privacy & cookie policy
             </div>
         </div>
 
-        </main>
+        <!-- Space under hero section -->
+        <div style="height: 50px;"></div>
+
+        <!-- Download buttons -->
+        <div class="download-buttons">
+            <a href="<?php echo get_template_directory_uri(); ?>/pdf/privacy_policy.pdf" class="download-button" download>
+                <span class="material-icons">get_app</span>
+                Download our Privacy Policy here
+            </a>
+            <a href="<?php echo get_template_directory_uri(); ?>/pdf/cookie_policy.pdf" class="download-button" download>
+                <span class="material-icons">get_app</span>
+                Download our Cookie Policy here
+            </a>
+        </div>
+
+    </main>
 </body>
 
 <?php get_footer(); ?>
