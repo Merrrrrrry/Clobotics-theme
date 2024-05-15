84cab9db6b024f0e00a561e17253e55223ded753
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-15 11:14:27 +0200

    st

--- page-career.php
+++ page-career.php
@@ -99,7 +99,7 @@ Template Name: Career
         <div style="height: 100px;"></div>
 
 
-        <!-- Image slide-show -->
+       <!-- Image slide-show -->
         <div class="images-career">
             <div class="image-slide">
                 <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-1.png" alt="career-img-1">
@@ -108,8 +108,6 @@ Template Name: Career
                 <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-4.png" alt="career-img-4">
                 <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-5.png" alt="career-img-5">
                 <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-6.png" alt="career-img-6">
-            </div>
-            <div class="image-slide">
                 <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-1.png" alt="career-img-1">
                 <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-2.png" alt="career-img-2">
                 <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-3.png" alt="career-img-3">
@@ -118,6 +116,7 @@ Template Name: Career
                 <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-6.png" alt="career-img-6">
             </div>
         </div>
+
 
         <?php
         $team = get_field('team');
