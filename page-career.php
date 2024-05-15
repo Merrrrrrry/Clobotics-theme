4f14eef1758896e613ad384cf357e2ffd81961b1
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-15 10:31:26 +0200

    img slide

--- page-career.php
+++ page-career.php
@@ -99,7 +99,7 @@ Template Name: Career
         <div style="height: 100px;"></div>
 
         <!-- Image carousel -->
-        <div class="carousel-container">
+        <div class="image-slide">
             <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-1.png" alt="career-img-1">
             <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-2.png" alt="career-img-2">
             <img src="<?php echo get_template_directory_uri(); ?>/media/career-img-3.png" alt="career-img-3">
