889296ff6f9e02c5c4295be9d26d7268a725749d
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-21 14:18:29 +0200

    fixing apply btn

--- js/javascript.js
+++ js/javascript.js
@@ -15,11 +15,11 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   
 
-// Function to scroll to the contact form section
+// Function to scroll to the contact form section when the Apply button is clicked
 function scrollToContactForm(event) {
     event.preventDefault(); 
-    var contactFormSection = document.getElementById("contact-form-section");
-    contactFormSection.scrollIntoView({ behavior: "smooth" });
+    var contactSection = document.getElementById("contact-form-container-career");
+    contactSection.scrollIntoView({ behavior: "smooth" });
 }
 
 // Event listener to the Apply button to scroll to the contact form section
@@ -32,23 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Company History Timeline JS
 
-document.addEventListener("DOMContentLoaded", function() {
-    const items = document.querySelectorAll(".company-history-item");
 
-    window.addEventListener("scroll", function() {
-        const triggerBottom = window.innerHeight / 5 * 4;
-
-        items.forEach(item => {
-            const itemTop = item.getBoundingClientRect().top;
-
-            if (itemTop < triggerBottom) {
-                item.classList.add("show");
-            } else {
-                item.classList.remove("show");
-            }
-        });
-    });
-});
 
 // Function for filtering REGIONs and JOB positons for "meet our team" on About us page
 
