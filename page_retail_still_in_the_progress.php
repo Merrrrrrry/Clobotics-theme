d7a4cbd3281a941e5aa9f61b1175122e4fdef12b
Merrrrrrry <grs.merry@gmail.com>
2024-05-22 15:34:10 +0200

    retail page

---
+++ page_retail_still_in_the_progress.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Template Name: Privacy & cookie policy
+*/
+?>
+
+<?php get_header(); ?>
+
+<body>
+    <main>
+
+    
+<!-- Hero section -->
+<div class="hero-section">
+    <div class="hero-section-background white_svg">
+        <img class="image-hero" src="<?php echo get_template_directory_uri(); ?>/media/Hero-imgs/Hero_retail_services.png" alt="Hero image">
+    </div>
+
+    <div class="hero-section-content">
+        <h1 class="title hero-title">Retail services</h1>
+        <p class="hero-slogan"> Ups!.. This page is stil in progress. <br>Please, select another one</p>
+    </div>
+</div>
+
+ <!-- Gap section -->
+ <div style="height: 40px;"></div>
+
+
+<!-- Gap section -->
+<div style="height: 40px;"></div>
+
+
+
+
+
+    </main>
+</body>    
+
+<?php get_footer(); ?>
+
+
