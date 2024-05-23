14806403a80416acd12b9ab4832c5116b3636a13
Merrrrrrry <grs.merry@gmail.com>
2024-05-23 15:02:07 +0200

    style last fucking step

--- page-home.php
+++ page-home.php
@@ -31,7 +31,7 @@ Template Name: Home
 <section id="two_main_directions_conteiner">
     <div class="retail_and_wind_content wind_content">
         <a href="http://maria-grysevych.dk/clobotics/home/wind-services/">
-            <h2 class="title">Wind services</h2>
+            <h2 class="title main_service_title">Wind services</h2>
             <p class="main_service_description"><?php the_field('wind_direction_description'); ?></p>
             <div class="direction_button container_to_center">
                 <div class="btn dark">Explore Clobotics Wind</div>  
@@ -40,7 +40,7 @@ Template Name: Home
     </div>
     <div class="retail_and_wind_content retail_content">
         <a href="http://maria-grysevych.dk/clobotics/home/retail-services/">
-            <h2 class="title">Retail services</h2>
+            <h2 class="title main_service_title">Retail services</h2>
             <p class="main_service_description"><?php the_field('retail_direction_description'); ?></p>
             <div class="direction_button container_to_center">
                 <div href="http://maria-grysevych.dk/clobotics/home/retail-services/"class="btn dark">Explore Clobotics Retail</div>  
