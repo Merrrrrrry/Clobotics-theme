309dc70a5a85d9ccfad55cc926100867828d5f13
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-24 14:41:44 +0200

    Test

--- single-wind-service.php
+++ single-wind-service.php
@@ -13,94 +13,120 @@ Template Name: single-wind-service
 <body>
 
 <main>
+<?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class(); ?>>
+        <h1 class="service-title"><?php the_field('service_title'); ?></h1>
 
-    <?php while (have_posts()) : the_post(); ?>
-        <article <?php post_class(); ?>>
-            <h1 class="service-title"><?php the_field('service_title'); ?></h1>
-
-            <div class="service-keypoints">
-                <?php if (have_rows('keypoints')) : ?>
-                    <?php while (have_rows('keypoints')) : the_row(); ?>
-                        <div class="keypoint">
-                            <h3><?php the_sub_field('keypoint_title'); ?></h3>
-                            <?php
-                            $icon = get_sub_field('keypoint_icon');
-                            if ($icon) : ?>
-                                <img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>">
-                            <?php endif; ?>
-                            <p><?php the_sub_field('keypoint_description'); ?></p>
-                        </div>
-                    <?php endwhile; ?>
+        <div class="service-keypoints">
+            <div class="keypoint">
+                <h3><?php the_field('1st_keypoint'); ?></h3>
+                <?php
+                $icon1_id = get_field('icon_of_the_keypoint_1');
+                if ($icon1_id) :
+                    $icon1 = wp_get_attachment_image_src($icon1_id, 'full');
+                    if ($icon1) : ?>
+                        <img src="<?php echo esc_url($icon1[0]); ?>" alt="<?php echo esc_attr(get_post_meta($icon1_id, '_wp_attachment_image_alt', true)); ?>">
+                    <?php endif; ?>
                 <?php endif; ?>
+                <p><?php the_field('1st_keypoint_subtitle_meta_description'); ?></p>
             </div>
+            <div class="keypoint">
+                <h3><?php the_field('2nd_keypoint'); ?></h3>
+                <?php
+                $icon2_id = get_field('icon_of_the_keypoint_2');
+                if ($icon2_id) :
+                    $icon2 = wp_get_attachment_image_src($icon2_id, 'full');
+                    if ($icon2) : ?>
+                        <img src="<?php echo esc_url($icon2[0]); ?>" alt="<?php echo esc_attr(get_post_meta($icon2_id, '_wp_attachment_image_alt', true)); ?>">
+                    <?php endif; ?>
+                <?php endif; ?>
+                <p><?php the_field('2nd_keypoint_subtitle_meta_description'); ?></p>
+            </div>
+            <div class="keypoint">
+                <h3><?php the_field('3rd_keypoint'); ?></h3>
+                <?php
+                $icon3_id = get_field('icon_of_the_keypoint_3');
+                if ($icon3_id) :
+                    $icon3 = wp_get_attachment_image_src($icon3_id, 'full');
+                    if ($icon3) : ?>
+                        <img src="<?php echo esc_url($icon3[0]); ?>" alt="<?php echo esc_attr(get_post_meta($icon3_id, '_wp_attachment_image_alt', true)); ?>">
+                    <?php endif; ?>
+                <?php endif; ?>
+                <p><?php the_field('3rd_keypoint_subtitle_meta_description'); ?></p>
+            </div>
+        </div>
 
-            <h2><?php the_field('short_article_title'); ?></h2>
-            <p><?php the_field('short_article_text'); ?></p>
-        </article>
-    <?php endwhile; ?>
+        <h2><?php the_field('short_article_title'); ?></h2>
+        <p><?php the_field('short_article_text'); ?></p>
+    </article>
+<?php endwhile; ?>
 
-    <section class="wind-call-to-action">
-        <img class="wind-call-to-action-image" src="<?php echo get_template_directory_uri(); ?>/media/contact-us.png" alt="Contact us img">
+
+
+<section class="wind-call-to-action">
+    <img  class="wind-call-to-action-image" src="<?php echo get_template_directory_uri(); ?>/media/contact-us.png" alt="Contact us img">
         <div class="content-wrapper">
             <h2>Optimise your<br>wind turbines</h2>
-            <a href="http://maria-grysevych.dk/clobotics/home/wind-services/contact/" class="btn optional">Contact us</a>
+            <button class="btn optional">Contact us</button>
         </div>
-    </section>
+</section>
 
-    <!-- Other wind services linking Section -->
-    <section class="wind_services_linking">
-        <h3 class="title">Discover Clobotics with our articles!</h3>
 
-        <div class="news_articles_linking_container owl-carousel owl-theme">
-            <?php $loop = new WP_Query(array('post_type' => 'wind-service', 'posts_per_page' => -1)); ?>
-            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-                <div class="service_single_container">
-                    <?php
-                    $image = get_field('service_main_image');
-                    if ($image) : ?>
-                        <img class="service_image_in_loop" src="<?php echo esc_url($image['url']); ?>" alt="Article image">
-                    <?php endif; ?>
-                    <h4 class="title"><?php the_field('service_title'); ?></h4>
 
-                    <div class="keypoints_container">
-                        <?php if (have_rows('keypoints')) : ?>
-                            <?php while (have_rows('keypoints')) : the_row(); ?>
-                                <?php
-                                $icon = get_sub_field('keypoint_icon');
-                                if ($icon) : ?>
-                                    <img class="icon" src="<?php echo esc_url($icon['url']); ?>" alt="Service keypoint icon">
-                                <?php endif; ?>
-                                <p class="subtitle"><?php the_sub_field('keypoint_description'); ?></p>
-                            <?php endwhile; ?>
-                        <?php endif; ?>
-                    </div>
-                </div>
-            <?php endwhile; wp_reset_query(); ?>
-        </div>
-    </section>
 
-    <?php 
-        wp_enqueue_script("jquery", "https://code.jquery.com/jquery-1.12.1.min.js");
-        wp_enqueue_script("carousel-script", get_stylesheet_directory_uri()."/js/plug-ins/OwlCarousel2/owl.carousel.min.js");
-    ?>
-    <script>
-        jQuery(document).ready(function($) {
-            $('.news_articles_linking_container').owlCarousel({
-                loop: true,
-                items: 3,
-                nav: true,
-                dots: true,
-                autoplay: true,
-                autoplayTimeout: 3000,
-                animateOut: 'fadeOut',
-                lazyLoad: true,
-            });
-        });
-    </script>
+
+    <!-- Other wind services linking  Section    Wind service -->
+
+ <section class="wind_services_linking">
+    <h3 class="title">Discover Clobotics with our articles!</h3>
+
+    <div class="news_articles_linking_container  owl-carousel owl-theme">
+
+        <?php $loop = new WP_Query( array( 'post_type' => 'wind-service', 'posts_per_page' => -1 ) ); ?>
+        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+            <div class="service_single_conteiner owl-carousel">
+                <img class="service_image_in_loop" src="<?php $image = get_field('service_main_image');  echo esc_url($image["url"]); ?>" alt="Article image" /> 
+                <h4 class="title"><?php the_field('service_title'); ?></h4>
+
+                <div class="keypoints_container">
+                    <img class="icon" src="<?php $image = get_field('icon_of_the_keypoint_1');  echo esc_url($image["url"]); ?>" alt="Service keypoint icon 1" /> 
+                    <p class="subtitle"><?php the_field('meta_description_short'); ?></p>  
+                    <img class="icon" src="<?php $image = get_field('icon_of_the_keypoint_2');  echo esc_url($image["url"]); ?>" alt="Service keypoint icon 2" /> 
+                    <p class="subtitle"><?php the_field('meta_description_short'); ?></p> 
+                    <img class="icon" src="<?php $image = get_field('icon_of_the_keypoint_3');  echo esc_url($image["url"]); ?>" alt="Service keypoint icon 3" /> 
+                    <p class="subtitle"><?php the_field('meta_description_short'); ?></p>  
+                </div>            
+            </div> 
+
+        <?php endwhile; wp_reset_query(); ?>
+    </div>
+
+</section>
+
+
+<?php 
+    wp_enqueue_script("jquery", "https://code.jquery.com/jquery-1.12.1.min.js");
+    wp_enqueue_script("carousel-script", get_stylesheet_directory_uri()."/js/plug-ins/OwlCarousel2/owl.carousel.min.js");
+?>
+<script>
+    jQuery(document).ready(function($) {
+        applyOwlCarousel({
+            loop: true,
+            items:3,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            animateOut: 'fadeOut',
+            lazyLoad: true,
+        })
+    });
+</script>
+
 
 </main>
    
 </body>
 <?php get_footer(); ?>
-
 
