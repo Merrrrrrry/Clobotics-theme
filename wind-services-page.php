359c0d68d7074bb1a5233559f648ddaf5d479604
Merrrrrrry <grs.merry@gmail.com>
2024-05-24 11:38:32 +0200

    hero

--- wind-services-page.php
+++ wind-services-page.php
@@ -16,7 +16,7 @@ Template Name: Wind services
 <!-- Hero section -->
 <div class="hero-section">
     <div class="hero-section-background white_svg">
-        <img class="image-hero" src="<?php echo get_template_directory_uri(); ?>/media/Hero-imgs/Hero_wind_services.jpg" alt="Hero image">
+        <img class="image-hero hero-wind-img" src="<?php echo get_template_directory_uri(); ?>/media/Hero-imgs/Hero_wind_services.jpg" alt="Hero image">
     </div>
 
     <div class="hero-section-content">
