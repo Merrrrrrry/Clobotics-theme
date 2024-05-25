68e64a9278fe0bacd4d996afcb79e017119e0731
Laura <72190730+HYUUMII@users.noreply.github.com>
2024-05-25 20:06:13 +0200

    T

--- page-contact.php
+++ page-contact.php
@@ -31,7 +31,7 @@ Template Name: Contact
     </div>
 </section>
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 <div class="background-white">
  <!-- Clobotics contact your office -->
